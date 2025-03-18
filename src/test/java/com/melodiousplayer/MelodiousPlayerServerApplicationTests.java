8da51045f971570eb3cd6fbe4b71f6cde5b4851a
LMINediva <3037907347@qq.com>
2025-03-18 22:07:53 +0800

    Fix config file database password leak bug.

--- src/test/java/com/melodiousplayer/MelodiousPlayerServerApplicationTests.java
+++ src/test/java/com/melodiousplayer/MelodiousPlayerServerApplicationTests.java
@@ -1,13 +1,27 @@
 package com.melodiousplayer;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MelodiousPlayerServerApplicationTests {
 
+
+    @Autowired
+    private StringEncryptor encryptor;
+
     @Test
-    void contextLoads() {
+    public void getPass() {
+        String url = encryptor.encrypt("jdbc:mysql://localhost:3306/database?useUnicode=true" +
+                "&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai");
+        String name = encryptor.encrypt("rds");
+        String password = encryptor.encrypt("rds");
+        System.out.println("mysql url: " + url);
+        System.out.println("mysql name: " + name);
+        System.out.println("mysql password: " + password);
+
     }
 
 }
